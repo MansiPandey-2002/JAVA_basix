da9d0ee1429209e9934df9b7be69e2ccbc4a9c8a
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-20 17:54:48 +0000

    Create Basic2.java

---
+++ Basic2.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class WordExample 
+{
+	private String strdata;
+	private String[] s;
+	private String occur;
+	private String nonoccur;
+	
+	public WordExample(String str) {
+		strdata = str;
+		s = strdata.split("\\s+");
+		occur = "";
+		nonoccur = ""; //otherwise null also taken
+	}
+	
+	public int countWord() {
+		int count = 0;
+		for (String i : s) 
+		{
+			char b = i.charAt(0);
+			char e = i.charAt(i.length() - 1);
+			if ((b=='a' || b=='e' || b=='i' || b=='o' || b=='u') && (e=='a' || e=='e' || e=='i' || e=='o' || e=='u')) 
+			{
+				count++;
+				occur += i + " ";
+			}
+			else
+			{
+			    nonoccur+=i+" ";
+			}
+		}
+		return count;
+	}
+	
+	public String placeWord() 
+	{
+		String result = " ";
+		result+=occur+nonoccur;
+		return result;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter sentence:");
+		String str = in.nextLine();
+		WordExample w = new WordExample(str);
+		System.out.println("Number of words with vowels at beginning and end: " + w.countWord());
+		System.out.println("After arrangement: " + w.placeWord());
+		in.close();
+	}
+}
